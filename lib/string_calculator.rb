44ed391c1994c8e11b00971843c3286e3cf18987
Sergei Bekrenev <sbekrenev@gmail.com>
2014-01-29 12:14:11 -0800

    Step 4

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,8 +6,14 @@ module StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     
-    numbers.split(%r{,|\n}).
-            tap{|n| raise "Delimiter violation" if n.include?('')}.
+    custom_delimiter_matches = /^\/\/(?<delimiter>.+)\n/.match(numbers)
+    unless custom_delimiter_matches.nil?
+      custom_delimiter = custom_delimiter_matches.captures.first
+      numbers.slice! "//#{custom_delimiter}\n"
+      custom_delimiter << '|'
+    end
+    
+    numbers.split(%r{#{custom_delimiter},|\n}).
             map{ |n| n.to_i }.
             inject(:+)   
   end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,9 @@ describe StringCalculator do
     it "handles new line characters '\n' as delimiters" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+    
+    it "handles custom delimiters" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
